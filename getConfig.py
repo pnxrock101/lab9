e280ee17ccf21f5cba06a875f195b026e35a3d83
Brent <pnxrock101@gmail.com>
2021-04-14 09:09:45 -0700

    Added additional error handling.

--- getConfig.py
+++ getConfig.py
@@ -1,5 +1,7 @@
 from netmiko import ConnectHandler
 from netmiko.ssh_exception import NetMikoTimeoutException
+from paramiko.ssh_exception import SSHExcetion
+from netmiko.ssh_exception import AuthenticationException
 from getpass import getpass
 
 user = input('Please enter SSH username: ')
@@ -16,5 +18,12 @@ try:
     connection = ConnectHandler(**ciscoDevice)
 except (NetMikoTimeoutException):
     print('The following device timed out: ' + ciscoDevice['host'])
-
+except (AuthenticationException):
+    print('Authentication failure on: ' + ciscoDevice['host'])
+except (SSHExcetion):
+    print('Could not connect to the device with SSH.  Check your SSH settings on: ' + ciscoDevice['host'])
+except (EOFError):
+    print('End of file while attempting: ' + ciscoDevice['host'])
+except Exception as other_error:
+    print('The error ' + str(other_error) + ' occured while connecting to: ' + ciscoDevice['host'])
 print('The script has completed')
